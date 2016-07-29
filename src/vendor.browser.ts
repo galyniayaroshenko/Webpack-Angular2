df810c164026c975c67e7221b21969c3dccfcec6
yaroshenko.g <yaroshenko270@gmail.com>
2016-07-29 10:33:18 +0300

    router

---
+++ src/vendor.browser.ts
@@ -0,0 +1,13 @@
+// Vendors
+
+// Angular 2
+import '@angular/platform-browser-dynamic';
+import '@angular/platform-browser';
+import '@angular/core';
+import '@angular/http';
+import '@angular/router';
+
+
+// RxJS 5
+// import 'rxjs/Rx';
+
